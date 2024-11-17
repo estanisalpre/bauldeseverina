46cfaf6880e938c1f118b191d82df65efa32afa8
Estanislao Salinas <estanisprevite@hotmail.com>
2024-11-17 12:10:25 -0500

    Botones del index.html listos

--- src/js/catalogo.js
+++ src/js/catalogo.js
@@ -1,1 +1,28 @@
 //Toda la funcionalidad de la pagina de catalogo
+
+document.addEventListener('DOMContentLoaded', () => {
+    //Nav buttons
+
+    //Burguer buttons
+
+    //Botones categoría
+    categoryButtons();
+})
+
+//Funcionalidad botones de categoría
+function categoryButtons(){
+    const categories = document.querySelectorAll('.toCategory')
+
+    categories.forEach(function(category){
+        category.addEventListener('click', () => {
+            const categoryId = category.dataset.id;
+
+            console.log(categoryId)
+            if(categoryId){
+                location.href = `catalogo.html#${categoryId}`;
+            } else {
+                console.log('Categoria sin data-id')
+            }
+        })
+    })
+}
