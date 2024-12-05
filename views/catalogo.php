d2c36de790c1ffff1d263f40a9b99af558c12bfd
Estanislao Salinas <estanisprevite@hotmail.com>
2024-12-05 11:52:37 -0500

    Changing styles and adding new feature in index

--- views/catalogo.php
+++ views/catalogo.php
@@ -328,6 +328,23 @@
                 ?>
             </div>
         </section>
+        <!--LOGIN-->
+        <section id="sectionForm" class="sectionForm">
+            <form action="login.php" method="POST" class="loginForm">
+                <span id="errorMessage">❌Credenciales incorrectas</span>
+                <p>Ingresa tus credenciales</p>
+                <input type="text" name="username" id="username" required placeholder="Usuario" maxlength="11">
+                <input type="password" name="password" id="password" required placeholder="Contraseña" maxlength="17">
+                <button id="submitLogin" type="submit">Ingresar</button>
+            </form>
+            <button id="backButton">Regresar</button>
+            <?php
+                // Mostrar error si las credenciales son incorrectas
+                if (isset($error)) {
+                    echo "<p>$error</p>";
+                }
+            ?>
+        </section>
     </main>
    <!--FOOTER-->
    <footer>
